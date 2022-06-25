c5725e68bf5e9861ebf35f8fbdd8c990c0733f5f
makifermis <makifermis@gmail.com>
2022-06-25 21:52:40 +0200

    plugin update

--- src/test/java/com/n11/runners/CukesRunner.java
+++ src/test/java/com/n11/runners/CukesRunner.java
@@ -6,7 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
         features = "src/test/resources/features",
         glue = "com/n11/step_definitions",
         dryRun = false,
