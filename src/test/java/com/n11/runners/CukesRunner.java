17a78aff627ae8d7b8fab4271f74f7e62514fa9e
makifermis <makifermis@gmail.com>
2022-06-26 19:11:19 +0200

    plugin update

--- src/test/java/com/n11/runners/CukesRunner.java
+++ src/test/java/com/n11/runners/CukesRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 //"pretty",
+                "json:target/cucumber.json",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
@@ -15,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/n11/step_definitions",
         dryRun = false,
-        tags = "@wip"
+
 )
 public class CukesRunner {
 }
