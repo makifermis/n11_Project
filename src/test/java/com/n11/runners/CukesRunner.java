b07a7e1997cdbb3ea4b8e14e0e1bd2f4c4a1e8e8
makifermis <makifermis@gmail.com>
2022-06-26 19:14:08 +0200

    plugin update

--- src/test/java/com/n11/runners/CukesRunner.java
+++ src/test/java/com/n11/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/n11/step_definitions",
-        dryRun = false,
+        dryRun = false
 
 )
 public class CukesRunner {
